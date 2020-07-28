0336f0b46942906aca755e665eaf5c203a0148a7
Abraham Park <65786838+parkjutsu@users.noreply.github.com>
2020-07-28 02:27:42 -0700

    Add option to set quantity when adding new ingredients and add new fragment to subtract used ingredients

--- PocketChef/app/src/main/java/cs446/uwaterloo/pocketchef/CookingActivity.java
+++ PocketChef/app/src/main/java/cs446/uwaterloo/pocketchef/CookingActivity.java
@@ -2,15 +2,25 @@ package cs446.uwaterloo.pocketchef;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 import cs446.uwaterloo.pocketchef.adapters.CookingTabsAdapter;
+import cs446.uwaterloo.pocketchef.adapters.IngredientUsageAdapter;
+import cs446.uwaterloo.pocketchef.model.Ingredient;
 import cs446.uwaterloo.pocketchef.model.Recipe;
-import cs446.uwaterloo.pocketchef.ui.cooking.CookingFragment;
+import cs446.uwaterloo.pocketchef.ui.cooking.IngredientUsageFragment;
 
 public class CookingActivity extends AppCompatActivity {
 
@@ -29,9 +39,23 @@ public class CookingActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         recipe = intent.getParcelableExtra("recipe");
+
+        TextView recipeName = findViewById(R.id.recipe_name);
+        recipeName.setText(recipe.getName());
     }
 
     public Recipe getRecipe() {
         return recipe;
+    }
+
+    // Called when the user clicks the "Consume Ingredients" button
+    public void openIngredientManager(View view) {
+        //setContentView(R.layout.fragment_ingredient_usage);
+
+        // Get list of Ingredients
+        ArrayList<Ingredient> ingredients = new ArrayList<>(recipe.getAllIngredients());
+
+        DialogFragment dialogFragment = new IngredientUsageFragment(ingredients);
+        dialogFragment.show(getSupportFragmentManager(), "ingredientDialog");
     }
 }
